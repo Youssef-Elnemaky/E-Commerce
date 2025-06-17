9ab12834e170b3ac2259ce47fafee6b863efcda8
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-17 04:19:29 +0300

    feat: add token model

---
+++ models/token.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const TokenSchema = new mongoose.Schema(
+    {
+        token: {
+            type: String,
+            required: [true, 'token is required'],
+        },
+        ip: {
+            type: String,
+            required: [true, 'ip is required'],
+        },
+        userAgent: {
+            type: String,
+            required: [true, 'user-agent is required'],
+        },
+        isValid: {
+            type: Boolean,
+            default: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'user is required'],
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Token', TokenSchema);
