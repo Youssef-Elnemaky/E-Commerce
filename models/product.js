e645535211054456219d5638a9ce75d88f134f59
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-20 10:20:29 +0300

    feat: add product model

---
+++ models/product.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            maxLength: [100, 'name cannot exceed 100 characters'],
+            minLength: [3, 'name must be at least 3 characters'],
+            required: [true, 'name is required'],
+        },
+        description: {
+            type: String,
+            trim: true,
+            maxLength: [500, 'description cannot exceed 100 characters'],
+            minLength: [3, 'description must be at least 3 characters'],
+            required: [true, 'description is required'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'price is required'],
+        },
+        slug: {
+            type: String,
+        },
+        ratingAvg: {
+            type: Number,
+        },
+        inStock: {
+            type: Boolean,
+            default: true,
+        },
+        stockQuantity: {
+            type: Number,
+            default: 0,
+        },
+        isFeatured: {
+            type: Boolean,
+            default: false,
+        },
+        image: String,
+        imageId: String,
+        tags: [String],
+    },
+    { timestamps: true }
+);
+
+ProductSchema.pre('save', function () {
+    this.slug = slugify(this.name);
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
