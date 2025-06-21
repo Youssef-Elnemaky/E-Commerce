244bec91abb76279d23f545742db592df3217f5b
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-21 21:41:37 +0300

    feat: add productController

---
+++ controllers/productController.js
@@ -0,0 +1,28 @@
+const { StatusCodes } = require('http-status-codes');
+const productService = require('../services/productService');
+
+exports.getAllProducts = async (req, res) => {
+    const products = await productService.getAllProducts(req.query);
+    res.status(StatusCodes.OK).json({ status: 'success', length: products.length, products });
+};
+
+exports.getProduct = async (req, res) => {
+    const productId = req.params.id;
+    const product = await productService.getProduct(productId);
+    res.status(StatusCodes.OK).json({ status: 'success', product });
+};
+
+exports.createProduct = async (req, res) => {
+    const product = await productService.createProduct(req.body);
+    res.status(StatusCodes.CREATED).json({ status: 'success', product });
+};
+
+exports.updateProduct = async (req, res) => {
+    const updatedProduct = await productService.updateProduct(req.params.id, req.body);
+    res.status(StatusCodes.OK).json({ status: 'success', updatedProduct });
+};
+
+exports.deleteProduct = async (req, res) => {
+    await productService.deleteProduct(req.params.id);
+    res.status(StatusCodes.NO_CONTENT).json({ status: 'success', msg: 'product deleted' });
+};
