59c2417e2441ecf1dc2b6ea7e0c6aceb8bff6fa3
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-22 08:06:57 +0300

    feat: attach a token to a cookie when the image is uploaded to check it later on while creating the product

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,12 +1,22 @@
 const { StatusCodes } = require('http-status-codes');
+const ms = require('ms');
 
 const uploadService = require('../services/uploadService');
-
+const attachToCookie = require('../utils/attachToCookie');
+const jwt = require('../utils/jwt');
 const uploadImage = async (req, res) => {
+    // local storage example
     // const uploadResult = await uploadService.uploadToLocal(req.file, 'upload-test');
+
+    // cloudinary storage (cloud)
     const uploadResult = await uploadService.uploadToCloudinary(req.file.buffer, process.env.CLOUDINARY_FOLDER_NAME);
-    console.log(uploadResult);
-    res.status(StatusCodes.OK).json({ status: 'success', uploadResult });
+
+    // signing the results with JWT
+    const uploadResultToken = await jwt.generateToken(uploadResult, ms(process.env.TEMP_IMAGE_TOKEN_LIFETIME) / 1000);
+    // attaching upload result token to a cookie
+    attachToCookie.attachSingle(res, uploadResultToken, 'uploadImageToken');
+
+    res.status(StatusCodes.OK).json({ status: 'success', msg: 'image uploaded' });
 };
 
 module.exports = { uploadImage };
