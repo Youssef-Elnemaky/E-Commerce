f81d0ffbb90b341d74a5f5535534d22b53d4118d
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-22 09:16:21 +0300

    feat: save the image url and public_id to the database after uploading

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -4,6 +4,8 @@ const ms = require('ms');
 const uploadService = require('../services/uploadService');
 const attachToCookie = require('../utils/attachToCookie');
 const jwt = require('../utils/jwt');
+const imageService = require('../services/imageService');
+
 const uploadImage = async (req, res) => {
     // local storage example
     // const uploadResult = await uploadService.uploadToLocal(req.file, 'upload-test');
@@ -11,10 +13,16 @@ const uploadImage = async (req, res) => {
     // cloudinary storage (cloud)
     const uploadResult = await uploadService.uploadToCloudinary(req.file.buffer, process.env.CLOUDINARY_FOLDER_NAME);
 
+    // attaching userId to createdBy
+    uploadResult.createdBy = req.user.userId;
+
     // signing the results with JWT
     const uploadResultToken = await jwt.generateToken(uploadResult, ms(process.env.TEMP_IMAGE_TOKEN_LIFETIME) / 1000);
     // attaching upload result token to a cookie
-    attachToCookie.attachSingle(res, uploadResultToken, 'uploadImageToken');
+    attachToCookie.attachSingle(res, uploadResultToken, 'imageToken');
+
+    // tracking the image in the DB
+    imageService.createImage(uploadResult);
 
     res.status(StatusCodes.OK).json({ status: 'success', msg: 'image uploaded' });
 };
