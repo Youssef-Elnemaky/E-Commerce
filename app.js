67430751a6ced5f13da7da469a93a139fa0b2333
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-28 08:58:14 +0300

    feat: add redistClient code

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookiePasrser = require('cookie-parser');
 // our own packages
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
+const redisClient = require('./utils/redisClient');
 
 // routers
 const authRouter = require('./routes/authRouter');
@@ -35,11 +36,21 @@ app.use('/api/v1/users', userRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-// connect to DB
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(console.log('Connected to DB...'))
-    .catch((err) => console.log(err));
-
 const PORT = 5000 || process.env.PORT;
-app.listen(5000, () => console.log(`Server is listening on port: ${PORT}...`));
+
+const startServer = async () => {
+    try {
+        // connect to DB
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to DB...');
+        // connect to Redis
+        await redisClient.connect();
+        console.log('Connected to redis...');
+        // make the server listen on PORT
+        app.listen(5000, () => console.log(`Server is listening on port: ${PORT}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
