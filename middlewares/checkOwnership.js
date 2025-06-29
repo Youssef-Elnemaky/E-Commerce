cbd31c9bdb066365d5a30e19f743783d499b2eeb
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-06-29 15:59:45 +0300

    feat: bypass checkownership if the user is admin and attach the resource to the req

--- middlewares/checkOwnership.js
+++ middlewares/checkOwnership.js
@@ -5,13 +5,19 @@ const checkOwnership = (Model) => {
         const resource = await Model.findById(req.params.id);
 
         if (!resource) {
-            throw new NotFoundError(`${resoruce.modelName} with id: ${req.params.id} not found`);
+            throw new NotFoundError(`${Model.modelName} with id: ${req.params.id} not found`);
         }
 
-        // check the ownership
-        if (resource.createdBy.toString() !== req.user.userId) {
+        // check if the user is an admin
+        const isAdmin = req.user.userRole === 'admin';
+        // normal user? check ownership
+        const isOwner = resource.createdBy.equals(req.user.userId);
+
+        if (!isAdmin && !isOwner) {
             throw new UnauthorizedError("you don't have permission to perform this action");
         }
+
+        req.resource = resource;
 
         next();
     };
