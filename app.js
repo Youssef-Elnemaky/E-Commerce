c1503b791cd7c84bf99e25f900e1ea9ebb2c3b33
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-07-01 23:16:09 +0300

    feat: add security packages

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const cookiePasrser = require('cookie-parser');
 
+//security packages
+const cors = require('cors');
+const helmet = require('helmet');
+const { xss } = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const expressMongoSanitize = require('@exortek/express-mongo-sanitize');
+
 // our own packages
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
@@ -20,25 +27,57 @@ const reviewRouter = require('./routes/reviewRouter');
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.FRONTEND_HOST,
+        credentials: true, // if you ever send cookies
+        exposedHeaders: ['Authorization'],
+    })
+);
+
+app.set('trust proxy', 1);
+const appLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: 'Too many requests from this IP. Please, try again in 15 minutes',
+    validate: {
+        trustProxy: 'acknowledged', // 🔥 Must be set if trust proxy is true
+    },
+});
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    message: 'Too many requests from this IP. Please, try again in 15 minutes',
+    validate: {
+        trustProxy: 'acknowledged', // 🔥 Must be set if trust proxy is true
+    },
+});
+
 // middlewares
+
 app.use(cookiePasrser(process.env.SIGNED_COOKIE_SECRET));
 app.use(express.json());
 app.use(express.static('./public'));
+app.use(helmet());
+app.use(xss());
+app.use(expressMongoSanitize());
 
 // routes
 app.get('/api/v1', (req, res) => {
     res.status(200).json({ status: 'success', msg: 'Hello World' });
 });
 
-app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', loginLimiter, authRouter);
+app.use('/api/v1/users', userRouter);
+app.use(appLimiter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/upload', uploadRouter);
-app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
@@ -49,7 +88,7 @@ const startServer = async () => {
         await redisClient.connect();
         console.log('Connected to redis...');
         // make the server listen on PORT
-        app.listen(5000, () => console.log(`Server is listening on port: ${PORT}...`));
+        app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}...`));
     } catch (error) {
         console.log(error);
     }
